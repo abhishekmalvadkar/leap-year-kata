71ec01b08397b224616ac47e93cde1edfca0b751
Abhishek <abhi@dev.com>
2026-03-09 11:30:57 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/lyk/LeapYearKataApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.lyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapYearKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapYearKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/lyk/LeapYearKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.lyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapYearKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
